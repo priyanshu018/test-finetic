a9282023e2c8ac6be7388252b5cf23d287ccf9d1
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Air.local>
2025-05-25 07:45:42 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    // Allow importing .mjs files from node_modules
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto',
-    });
-    return config;
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
