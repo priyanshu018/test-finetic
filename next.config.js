6aa16403083d4ee5a0cfe53d5d494fb7bc5c3ebf
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Air.local>
2025-05-25 07:44:23 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: process.env.NODE_ENV === 'production' ? 'app' : '.next',
   trailingSlash: true,
   images: {
     unoptimized: true,
