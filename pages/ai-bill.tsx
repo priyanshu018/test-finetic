087500153b119625a4a86fd4039c823f3549285f
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Air.local>
2025-06-29 17:23:50 +0530

    changes

--- pages/ai-bill.tsx
+++ pages/ai-bill.tsx
@@ -632,9 +632,7 @@ export default function BillWorkflow() {
   const [netAmountTotal, setNetAmountTotal] = useState<number>(0);
   const [gstTotals, setGstTotals] = useState<{ [key: string]: number }>({});
   const [companyList, setCompanyList] = useState([
-    "Prime Depth Labs",
-    "Test 1",
-    "Test 2",
+    "No Companies Avaialble please select a company in tally"
   ]);
   const [isWithinState, setIsWithinState] = useState(false);
   const [selectedCompanyName, setSelectedCompanyName] = useState("");
@@ -1520,8 +1518,13 @@ export default function BillWorkflow() {
   };
 
   useEffect(() => {
-    fetchCompanies();
-    fetchGstDetails();
+    const interval = setInterval(() => {
+      fetchCompanies();
+      fetchGstDetails();
+    }, 3000); // 2000ms = 2 seconds
+
+    // Clean up the interval when selectedCompanyName changes or component unmounts
+    return () => clearInterval(interval);
   }, [selectedCompanyName]);
 
   useEffect(() => {
@@ -1605,7 +1608,6 @@ export default function BillWorkflow() {
   }, [billData, currentBillIndex]);
 
   const fetchCompanies = async () => {
-    setIsLoading(true);
 
     const xmlData = `<ENVELOPE>
       <HEADER>
@@ -1934,24 +1936,7 @@ export default function BillWorkflow() {
             )}
 
             {/* --- GST Number Field --- */}
-            <div className="mb-6 px-8 text-black">
-              <div>
-                <label
-                  htmlFor="gstNumber"
-                  className="block text-sm font-medium text-black"
-                >
-                  GST Number
-                </label>
-                <input
-                  type="text"
-                  id="gstNumber"
-                  value={gstNumber}
-                  onChange={(e) => setGstNumber(e.target.value)}
-                  placeholder="Enter GST Number"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-              </div>
-            </div>
+
 
             <div className="flex justify-end my-6 px-4">
               <button
